cd16285a7692613e24f18d9dd5c70d15d071aa9f
Adrian Zelada Torrez <zelada_torrez@hotmail.com>
2020-10-09 00:26:37 -0400

    IMP: request layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { firebaseConfig } from '../environments/firebase-config';
 import { HttpClientModule} from '@angular/common/http';
+import { RepositoryData, ModelConnection} from './interfaces/repository';
+
+export function RequestProvider(
+  service: RepositoryData,
+  options: ModelConnection
+) {
+  return new RequestService(service, options);
+}
 
 @NgModule({
   declarations: [
