2d31fec9ec1302a89281c1ea871fdadacf51606d
Adrian Zelada Torrez <zelada_torrez@hotmail.com>
2020-10-09 00:26:04 -0400

    IMP: request layer

--- src/app/http/http-request.ts
+++ src/app/http/http-request.ts
@@ -1,27 +1,38 @@
 import { HttpClient} from '@angular/common/http';
-import { ModelConnection} from '../interfaces/repository';
+import { ModelConnection, RepositoryData} from '../interfaces/repository';
 
+const HOST = 'https://api.first.org/v1/';
 export class HttpService implements RepositoryData{
   constructor(private http: HttpClient, private config: ModelConnection) {}
 
   getData() {
-    return this.http.get(`${this.config.model}`);
+    return this.http.get(`${HOST}/${this.config.model}`);
   }
 
   filter(query: any) {
-    return this.http.get(`${this.config.model}`, );
+    const params = this.stringifyUrl(query);
+    return this.http.get(`${HOST}/${this.config.model}?${params}`);
   }
 
   add(data: any) {
-
+    return this.http.post(`${HOST}/${this.config.model}`,data);
   }
 
   update(data: any) {
-
+    return this.http.post(`${HOST}/${this.config.model}/${data.id}`,data);
   }
 
   remove(id: string) {
+    return this.http.delete(`${HOST}/${this.config.model}/${id}`);
+  }
 
+
+  stringifyUrl(data: any = {}) {
+    const params = Object.entries(data);
+    const paramsString = params.map((item) => {
+      return item.join('=');
+    });
+    return paramsString.join('&');
   }
 }
 // https://api.first.org/v1/get-countries
---
+++ src/app/services/request-service.ts
@@ -0,0 +1,12 @@
+import { ɵINJECTOR_IMPL__POST_R3__ } from "@angular/core";
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+
+  constructor() { }
+
+}
